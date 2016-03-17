fabf9d0f935fd733ab6ca7a718590a87a67685de
awalk1252 <awalk1252@withs.org>
2016-03-17 14:52:04 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -1,6 +1,11 @@
 $(document).ready(function()
 {
-	$('#message').focus();
-
-	// YOUR CODE GOES HERE
+	$("#message").focus();
+	$("#post-btn").click(function() {
+	    
+	
+		var nameText = $("#message").val();
+		$("#wall").append("<div>" + "<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + nameText +"</div>" );
+	
+	});	
 });
